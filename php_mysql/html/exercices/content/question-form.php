938f824a8cc096e28fcfe36b5ee354f1ec0edfa1
Olivia LINSTRUMELLE <olinstru@gmail.com>
2023-11-23 10:29:01 +0100

    small changes to form

--- php_mysql/html/exercices/content/question-form.php
+++ php_mysql/html/exercices/content/question-form.php
@@ -1,10 +1,10 @@
 <form action="">
     <h1>Poser une question</h1>
     <label for="title">Titre de la publication :</label><br>
-    <input type="text" placeholder="Titre...">
+    <input name="title" type="text" placeholder="Titre...">
     <br>
     <label for="text">Contenu de la publication :</label><br>
     <textarea name="question" cols="100" rows="15" placeholder="Posez votre question ici..."></textarea>
     <br>
-    <input type="button" value="Soumettre question">
+    <input type="submit" value="Soumettre question">
 </form>
