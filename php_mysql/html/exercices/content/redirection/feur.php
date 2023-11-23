1781fa0ad52e914b76666c48cbb3352c6d7c2733
Romain🔥 <romainthibaud45@gmail.com>
2023-11-23 11:00:13 +0100

    feat(feur): animated feurs

--- php_mysql/html/exercices/content/redirection/feur.php
+++ php_mysql/html/exercices/content/redirection/feur.php
@@ -11,3 +11,45 @@
         <input type="submit" value="Retourner à l'accueil">
     </form>
 </div>
+<script>
+    function createFeurs() {
+        var container = document.getElementById("redirection-feur");
+
+        for (var i = 0; i < 10; i++) {
+            var feur = document.createElement("p");
+            feur.textContent = "feur";
+            feur.className = "floating-feur";
+            feur.style.fontFamily = "Hachi Maru Pop";
+            feur.style.position = "absolute";
+            feur.style.color = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
+            feur.style.left = Math.random() * window.innerWidth + "px";
+            feur.style.top = Math.random() * window.innerHeight + "px";
+            container.appendChild(feur);
+        }
+
+
+        setInterval(function() {
+            var feurs = document.getElementsByClassName("floating-feur");
+            for (var i = 0; i < feurs.length; i++) {
+                feurs[i].style.left = Math.random() * window.innerWidth + "px";
+                feurs[i].style.top = Math.random() * window.innerHeight + "px";
+                feurs[i].animate([
+                    // keyframes
+                    {
+                        opacity: 1,
+                        transform: 'scale(1.5)'
+                    },
+                    {
+                        opacity: 0,
+                        transform: 'scale(0.5)'
+                    }
+                ], {
+                    // timing options
+                    duration: 2000,
+                    iterations: Infinity
+                });
+            }
+        }, 2000);
+    }
+    createFeurs()
+</script>
