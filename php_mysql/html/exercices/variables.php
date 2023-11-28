1b673ae3c4452012251fcac31293dc1f7cdaf8a2
Olivia LINSTRUMELLE <olinstru@gmail.com>
2023-11-28 18:51:24 +0100

    changes

--- php_mysql/html/exercices/variables.php
+++ php_mysql/html/exercices/variables.php
@@ -19,5 +19,5 @@ $answers = [
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim sapien ac augue maximus, at varius tellus vehicula. Nulla ut dui augue. Fusce vitae posuere sem. Aliquam ornare vulputate libero. Quisque elementum, urna vitae ullamcorper venenatis, enim odio porta erat, vitae cursus urna quam quis augue. Maecenas ac laoreet metus.',
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus accumsan nec nisi a varius. Sed rutrum vehicula nisl a imperdiet. Donec ultrices ultricies vehicula. Etiam id malesuada neque, non eleifend risus. Nunc blandit, risus ac egestas tristique, dui leo condimentum sem, ultricies congue augue tortor id metus. Etiam eget eros.',
   'BON',
-  'JOUR'
+  'JOUR',
 ];
