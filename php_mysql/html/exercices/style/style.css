c25a55e1f389a145f72e36adaf208cbe3f626f1b
Louis-maucourt <louis.maucourt7@gmail.com>
2023-11-30 02:20:58 +0100

    Louis

---
+++ php_mysql/html/exercices/style/style.css
@@ -0,0 +1,23 @@
+<?php
+
+$title = 'TITRE DE LA QUESTION';
+
+$timestamp = 1700131242;
+$date = date('d/m/y H:i', $timestamp);
+$author = 'Michel Morin';
+
+$question =
+  '<p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis lectus tempor, tincidunt sem eu, tristique nulla. Cras blandit blandit accumsan. Proin scelerisque magna mi, quis hendrerit nibh commodo aliquam. Donec pellentesque tortor dolor, nec porta mi aliquet quis. Ut ac posuere eros, in aliquam purus. Proin id placerat erat. Integer ullamcorper elit a tincidunt feugiat. Ut mattis nec turpis id iaculis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras mattis orci quis velit mollis fermentum. Duis elementum purus quam, sed lobortis sem pulvinar at. Etiam iaculis eget ante eu convallis. Donec efficitur mi at massa bibendum placerat ut sit amet elit.
+  
+    Suspendisse potenti. Aliquam laoreet lobortis accumsan. Quisque euismod quam est, vitae tempus lacus mattis vel. Vivamus quis orci nulla. Maecenas id placerat purus, sit amet cursus sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse gravida congue eleifend. Nunc tincidunt, est id tincidunt ultrices, metus ipsum consectetur sapien, vel luctus libero ligula vel risus. Fusce sagittis ut mi vel rhoncus. Aenean tortor dolor, euismod ut semper et, pharetra nec nunc.
+  
+    Phasellus eget blandit neque, eu mattis purus. Donec sit amet hendrerit neque. Pellentesque facilisis sollicitudin tortor, ut ullamcorper lacus lobortis nec. Sed lectus lectus, ornare a porttitor non, gravida ac metus. Ut maximus lectus eu vehicula cursus. Donec pulvinar imperdiet lectus, eget facilisis turpis tincidunt sed. Donec iaculis malesuada urna, ut bibendum turpis luctus maximus. In a consectetur odio. Nullam iaculis rhoncus posuere. Suspendisse eget sapien felis. Donec commodo purus sed ligula fringilla volutpat.
+  </p>';
+
+$answers = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim sapien ac augue maximus, at varius tellus vehicula. Nulla ut dui augue. Fusce vitae posuere sem. Aliquam ornare vulputate libero. Quisque elementum, urna vitae ullamcorper venenatis, enim odio porta erat, vitae cursus urna quam quis augue. Maecenas ac laoreet metus.',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus accumsan nec nisi a varius. Sed rutrum vehicula nisl a imperdiet. Donec ultrices ultricies vehicula. Etiam id malesuada neque, non eleifend risus. Nunc blandit, risus ac egestas tristique, dui leo condimentum sem, ultricies congue augue tortor id metus. Etiam eget eros.',
+  'BON',
+  'JOUR'
+];
