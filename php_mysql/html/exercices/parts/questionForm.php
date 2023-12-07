cbd99b09e5ca40bdf56ab621e439da4a5145f036
Clément Tutin <clement.tutin.pro@gmail.com>
2023-12-07 09:09:26 +0100

    Commit cours 30//11

--- php_mysql/html/exercices/parts/questionForm.php
+++ php_mysql/html/exercices/parts/questionForm.php
@@ -1,3 +1,27 @@
+<?php
+session_start();
+
+// Vérifier si le formulaire est soumis
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Récupérer la réponse du formulaire
+    $reponse = $_POST['reponse'];
+
+    // Stocker la réponse dans la session
+    $_SESSION['reponses'][] = $reponse;
+}
+
+// Vérifier si une question est sélectionnée
+if (isset($_POST['changer_question'])) {
+    // Effacer les réponses lorsque la question est changée
+    unset($_SESSION['reponses']);
+}
+
+// Afficher les réponses s'il y en a
+
+$reponses = isset($_SESSION['reponses']) ? $_SESSION['reponses'] : [];
+
+?>
+
 <section id="questionForm">
     <form action="/exercices/processQuestion.php" method="post">
         <label for="question">Question :</label>
