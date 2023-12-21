da66a2bfc7b83cbb7a914803e4f18369191d9f3a
Georgina Berrezel <georgina.berrezel@gmail.com>
2023-12-21 09:45:51 +0100

    test migration file

---
+++ php_mysql/html/exercices/bdd/2023-12-21_v0.4_create_answer_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE answer
+(
+    id      INTEGER      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    content LONGTEXT     NOT NULL,
+    author  VARCHAR(255) NOT NULL,
+    date    TIMESTAMP    NOT NULL
+);
---
+++ php_mysql/html/exercices/bdd/migration.php
@@ -0,0 +1,3 @@
+<?php
+
+$dbh = new PDO('mysql:host=mysql', "roote", "mariadb_password");
