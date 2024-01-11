34545d9aacc5a6e5a4f5612f3570f1cf13380299
Olivia LINSTRUMELLE <olinstru@gmail.com>
2024-01-11 11:56:32 +0100

    updated data.sql

--- php_mysql/html/exercices/bdd/data.sql
+++ php_mysql/html/exercices/bdd/data.sql
@@ -1,5 +1,6 @@
+TRUNCATE question;
+TRUNCATE answer;
 DELETE FROM author;
-ALTER TABLE author AUTO_INCREMENT = 1;
 
 INSERT INTO author (name, email)
 VALUES
@@ -14,12 +15,19 @@ VALUES
 ("Maxime", "maximus@gmail.com"),
 ("Sylvestrine", "sylvestrinecoeur@gmail.com");
 
-DELETE FROM question;
-ALTER TABLE question AUTO_INCREMENT = 1;
-
 INSERT INTO question
 (title, content, author_id, date)
 VALUES
-("Qui est Henry IV ?", "Lroemen ototot dsjhfgsd", 5, "2023-01-12"),
-("Qui rouge ?", "Lroemen ototot dsjhfgsd", 4, "2023-11-14"),
-("Est-ce la HESS?", "Lroemen ototot dsjhfgsd", 2, "2023-04-12");
+("Qui est Henry IV ?", "Lroemen ototot dsjhfgsd", (SELECT id FROM author ORDER BY RAND() LIMIT 1), "2023-01-12"),
+("Quel légume est rouge ?", "Lroemen ototot dsjhfgsd", (SELECT id FROM author ORDER BY RAND() LIMIT 1), "2023-11-14"),
+("Est-ce la HESS?", "Lroemen ototot dsjhfgsd", (SELECT id FROM author ORDER BY RAND() LIMIT 1), "2023-04-12");
+
+INSERT INTO answer
+(content, date, author_id, question_id)
+VALUES
+("C'est un roi français", "2021-12-03", (SELECT id FROM author ORDER BY RAND() LIMIT 1), 1),
+("Un poivron", "2024-01-10", (SELECT id FROM author ORDER BY RAND() LIMIT 1), 2),
+("Koikoubé", "2022-05-21", (SELECT id FROM author ORDER BY RAND() LIMIT 1), 3),
+("C'est celui après Henri III", "2023-12-31", (SELECT id FROM author ORDER BY RAND() LIMIT 1), 1),
+("Une tomate", "2021-09-30", (SELECT id FROM author ORDER BY RAND() LIMIT 1), 2),
+("42", "2023-02-11", (SELECT id FROM author ORDER BY RAND() LIMIT 1), 3)
